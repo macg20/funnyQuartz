993fb3976c6c230df528fa6f1b2fbdf6251848a8
kossss1 <kossss1@o2.pl>
2017-08-20 19:35:59 +0200

    feature/develop

--- services/src/main/java/pl/funnyqrz/services/pdf/PDFReportRenderer.java
+++ services/src/main/java/pl/funnyqrz/services/pdf/PDFReportRenderer.java
@@ -1,8 +1,13 @@
 package pl.funnyqrz.services.pdf;
 
+import com.itextpdf.text.DocumentException;
+import org.springframework.stereotype.Service;
+
 import java.io.File;
+import java.io.FileNotFoundException;
+
 
 public interface PDFReportRenderer {
 
-    File renderReport();
+    File renderReport() throws FileNotFoundException, DocumentException;
 }

5fd3a65bfbff20179995342d8a1d3638075eb289
kossss1 <kossss1@o2.pl>
2017-12-19 23:30:53 +0100

    feature/email_rebuild

--- services/src/main/java/pl/funnyqrz/services/aop/EmailAspectService.java
+++ services/src/main/java/pl/funnyqrz/services/aop/EmailAspectService.java
@@ -4,24 +4,80 @@ import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import pl.funnyqrz.repositories.ExchangeRateRepository;
 import pl.funnyqrz.services.AbstractService;
+import pl.funnyqrz.services.email.EmailService;
+import pl.funnyqrz.services.email.MessageService;
+
+import javax.mail.MessagingException;
+import java.io.IOException;
+import java.util.Collections;
 
 @Aspect
-@Component
-public class EmailAspectService extends AbstractService{
+@Service
+public class EmailAspectService extends AbstractService {
 
+    private EmailService emailService;
+    private MessageService messageService;
+    private ExchangeRateRepository exchangeRateRepository;
+//    private PDFReportRenderer pdfReportRenderer;
+
+    @Autowired
+    public EmailAspectService(EmailService emailService, MessageService messageService, ExchangeRateRepository exchangeRateRepository) {
+        this.emailService = emailService;
+        this.messageService = messageService;
+        this.exchangeRateRepository = exchangeRateRepository;
+//        this.pdfReportRenderer = pdfReportRenderer;
+    }
 
     @Before("execution(* pl.funnyqrz.services.nbp.NbpServiceImpl.downloadAndSaveExchangeRate(..))")
     public void logBeforeAllMethodsJobExecute1(JoinPoint joinPoint) {
-       getLogger().info("Executing method:" + joinPoint.getSignature().getName());
+        getLogger().info("Executing method:" + joinPoint.getSignature().getName());
     }
     //TODO - afer executing downloadAndSaveExchangeRate method-> send email with reports( for example, velocity, fremarker template)
 
     @Transactional
     @After("execution(* pl.funnyqrz.services.nbp.NbpServiceImpl.downloadAndSaveExchangeRate(..))")
-    public void sendEmailAfterDownloadAndSaveExchangeRate(JoinPoint joinPoint) {
+    public void sendEmailWithReportAfterDownloadAndSaveExchangeRate(JoinPoint joinPoint) throws IOException, MessagingException {
 
+//        File report = null;
+//        ExchangeRateEntity lastExchangeRateEntity = null;
+//
+//        try {
+//
+//             lastExchangeRateEntity = findLastExchangeRateEntity();
+//
+//        } catch (NotFoundDatabaseRecord notFoundDatabaseRecord) {
+//            getLogger().error("Error occured!", notFoundDatabaseRecord);
+//        }
+//
+//        try {
+//
+//            report = pdfReportRenderer.renderReport(lastExchangeRateEntity);
+//
+//        } catch (FileNotFoundException e) {
+//            getLogger().error("Error occurred!", e);
+//        } catch (DocumentException e) {
+//            getLogger().error("Error occurred!", e);
+//        }
+//        // TODO user register and get users addresses
+//
+//        Set<EmailAddress> addresses = new HashSet<>();
+//        addresses.add(new EmailAddress("aaaaaa@gmail.com"));
+//
+//        MimeMessage mimeMessage = messageService.createMessageWithReport(NameUtils.createDefaultMessageTitleWithDate(),
+//                "DESCRIPTION",addresses, report);
+//
+//        emailService.sendMessage(mimeMessage);
+
+        emailService.sendMessage ("TEST","TEST",Collections.emptyList(), Collections.emptyList());
     }
+
+//    private ExchangeRateEntity findLastExchangeRateEntity() throws NotFoundDatabaseRecord {
+//        Optional<ExchangeRateEntity> exchangeRateEntity = Optional.ofNullable(exchangeRateRepository.findLast());
+//        return exchangeRateEntity.orElseThrow(() -> new NotFoundDatabaseRecord("Not found echange rate in database"));
+//    }
 }
---
+++ services/src/main/java/pl/funnyqrz/services/email/MessageServiceImpl.java
@@ -0,0 +1,62 @@
+package pl.funnyqrz.services.email;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+import pl.funnyqrz.services.AbstractService;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.File;
+import java.util.Collection;
+
+@Service
+public class MessageServiceImpl extends AbstractService implements MessageService {
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    @Value("${email.from}")
+    private String from;
+
+    @Autowired
+    public MessageServiceImpl(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+
+    @Override
+    public MimeMessage createMessage(String subject, String content, String footer, Collection<File> attachments) throws MessagingException {
+
+        MimeMessage message = javaMailSender.createMimeMessage();
+//        Multipart multiPart = new MimeMultipart("alternative");
+//
+//        MimeBodyPart textPart = new MimeBodyPart();
+//        textPart.setText(content, "utf-8");
+//
+//        MimeBodyPart footerPart = new MimeBodyPart();
+//        textPart.setText(footer, "utf-8");
+//
+//        MimeBodyPart attachmentPart = new MimeBodyPart();
+//        attachments.forEach(file -> {
+//            try {
+//                attachmentPart.attachFile(file);
+//            } catch (IOException e) {
+//                getLogger().error("Cannot add attachment");
+//            } catch (MessagingException e) {
+//                getLogger().error("Cannot add attachment");
+//            }
+//        });
+
+//        multiPart.addBodyPart(textPart);
+//        multiPart.addBodyPart(footerPart);
+//        multiPart.addBodyPart(attachmentPart);
+//        message.setContent(multiPart);
+        message.setText(content);
+//        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(message);
+
+        return message;
+    }
+
+}

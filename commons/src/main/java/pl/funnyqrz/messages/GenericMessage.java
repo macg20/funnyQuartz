72bdc5eac88aefabda530ff2ba1fb8d8c269ed5d
kossss1 <kossss1@o2.pl>
2018-01-15 20:23:13 +0100

    messages changes

--- commons/src/main/java/pl/funnyqrz/messages/GenericMessage.java
+++ commons/src/main/java/pl/funnyqrz/messages/GenericMessage.java
@@ -1,28 +1,22 @@
 package pl.funnyqrz.messages;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class GenericMessage<T> {
 
-    private Set<T> messages;
-
-
+    private T messages;
 
     public GenericMessage() {
-        if(messages == null)
-            messages = new HashSet<>();
+
     }
 
-    public GenericMessage(Set<T> messages) {
+    public GenericMessage(T messages) {
         this.messages = messages;
     }
 
-    public Set<T> getMessages() {
+    public T getMessages() {
         return messages;
     }
 
-    public void setMessages(Set<T> messages) {
+    public void setMessages(T messages) {
         this.messages = messages;
     }
 }
--- commons/src/main/java/pl/funnyqrz/messages/SystemMessage.java
+++ commons/src/main/java/pl/funnyqrz/messages/SystemMessage.java
@@ -1,9 +1,7 @@
 package pl.funnyqrz.messages;
 
-import java.util.Set;
-
 public interface SystemMessage {
 
-    final String USER_CORRECTLY_REGISTER = "An activation email has been sent to your address";
-    final String  USER_ALREADY_EXISTS = "User already register!";
+    String USER_CORRECTLY_REGISTER = "An activation email has been sent to your address";
+    String USER_ALREADY_EXISTS = "User already register!";
 }

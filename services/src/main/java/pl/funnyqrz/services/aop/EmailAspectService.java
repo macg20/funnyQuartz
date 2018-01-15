0f03b6f7cd18000481d92e72ef3822d389af890b
kossss1 <kossss1@o2.pl>
2018-01-15 20:25:39 +0100

    changes method name add todo list

--- services/src/main/java/pl/funnyqrz/services/aop/EmailAspectService.java
+++ services/src/main/java/pl/funnyqrz/services/aop/EmailAspectService.java
@@ -8,11 +8,9 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.CollectionUtils;
 import pl.funnyqrz.entities.ExchangeRateEntity;
 import pl.funnyqrz.exceptions.NotFoundDatabaseRecord;
-import pl.funnyqrz.repositories.ExchangeRateRepository;
 import pl.funnyqrz.services.AbstractService;
 import pl.funnyqrz.services.account.UserService;
 import pl.funnyqrz.services.email.EmailService;
@@ -40,22 +38,24 @@ public class EmailAspectService extends AbstractService {
 
 
     @Before("execution(* pl.funnyqrz.services.nbp.NbpServiceImpl.downloadAndSaveExchangeRate(..))")
-    public void logBeforeAllMethodsJobExecute1(JoinPoint joinPoint) {
+    public void logBeforeDownloadAndSaveExchangeRate(JoinPoint joinPoint) {
         getLogger().info("Executing method:" + joinPoint.getSignature().getName());
     }
 
-    @Transactional
+
     @AfterReturning(value = "execution(* pl.funnyqrz.services.nbp.NbpServiceImpl.downloadAndSaveExchangeRate(..))", returning = "result")
-    public void sendEmailWithReportAfterDownloadAndSaveExchangeRate(JoinPoint joinPoint,Object result) throws IOException, MessagingException {
-        getLogger().error("Start aspect.");
-        File report = pdfReportRenderer.renderReport((ExchangeRateEntity)result);
+    public void generateReportAfterDownloadedExchangeRateAndSendEmialForUsers(JoinPoint joinPoint, Object result) throws IOException, MessagingException {
+        getLogger().error("Start aspect..");
+        File report = pdfReportRenderer.renderReport((ExchangeRateEntity) result);
         Set<File> attachments = Sets.newHashSet(report);
         Set<String> emailAddresses = findAllEmails();
+        //TODO save reports in db
+        //TODO render content
         emailService.sendMessage("TEST", "TEST", emailAddresses, attachments);
     }
 
     private Set<String> findAllEmails() {
-        Set<String> emails = userService.findAllUserEmail();
+        Set<String> emails = userService.findAllEmails();
         if (!CollectionUtils.isEmpty(emails))
             return emails;
         else
